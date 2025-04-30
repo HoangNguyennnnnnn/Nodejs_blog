55fa212810f496b6dcbd0a92066894325836e08b
Hoàng Nguyên <nthoangnguyen1308@gmail.com>
2025-04-30 10:41:35 +0700

    Static file & SCSS

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(morgan('combined'));
 
 // Cấu hình engine template
