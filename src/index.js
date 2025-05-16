170a34b7723b36c38691d860e411a8fbe7e1a016
Hoàng Nguyên <nthoangnguyen1308@gmail.com>
2025-05-16 23:45:13 +0700

    Update courses

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const path = require('path');
 
 const app = express();
@@ -20,8 +21,14 @@ app.use(express.json());
 
 app.use(morgan('combined'));
 
+app.use(methodOverride('_method'));
 // Cấu hình engine template
-app.engine('hbs', engine({ extname: '.hbs' }));
+app.engine('hbs', engine({
+    extname: '.hbs',
+    helpers: {
+        sum: (a, b) => a + b,
+    },
+}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
